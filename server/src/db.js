b7c08ff1c7783b21cc7b78feddfe99df70479cfc
daprionil <davidarenaoflol23@gmail.com>
2023-09-08 20:48:39 -0500

    Set configuration db with mongo

--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-const { MONGO_USERNAME, MONGO_USER_PASSWORD } = process.env;
 
 const conectarDB = async () => {
-    const gist = `mongodb+srv://${MONGO_USERNAME}:${MONGO_USER_PASSWORD}@cluster0.4fyrspp.mongodb.net/?retryWrites=true&w=majority`;
+    const { MONGO_USERNAME, MONGO_USER_PASSWORD } = process.env;
+    
+    const gist = `mongodb+srv://${MONGO_USERNAME}:${MONGO_USER_PASSWORD}@cluster0.wkllydk.mongodb.net/?retryWrites=true&w=majority`;
     const conn = await mongoose.connect(gist,{
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -12,7 +13,7 @@ const conectarDB = async () => {
     const { host, port } = conn.connection;
     const url = `${host}:${port}`;
     console.log('MongoDB conectado en: ' + url);
-}
+};
 
 
 module.exports = conectarDB;
