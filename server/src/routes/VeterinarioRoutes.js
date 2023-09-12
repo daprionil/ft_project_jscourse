18008feb8b77870c74f82c0fca8859f1a3f10ef1
daprionil <davidarenaoflol23@gmail.com>
2023-09-12 07:25:32 -0500

    Set middleware to get Bearer Token

--- server/src/routes/VeterinarioRoutes.js
+++ server/src/routes/VeterinarioRoutes.js
@@ -1,13 +1,16 @@
 const { Router } = require('express');
 const { register, profile, confirmAccount, authVeterinario } = require('../handlers/handlerVeterinarios.js');
+const authMiddleware = require('../helpers/authMiddleware.js');
 
 const veterinarioRouter = Router();
 
+//? Crea un usuario de veterinario
 veterinarioRouter.post('/', register);
+//? Valida al usuario con password y email
 veterinarioRouter.post('/login', authVeterinario);
-
+//? Confirma la cuenta de un veterinario por medio de su Id
 veterinarioRouter.get('/confirm/:tokenId', confirmAccount);
-
-veterinarioRouter.get('/profile', profile)
+//? 
+veterinarioRouter.get('/profile', authMiddleware, profile);
 
 module.exports = veterinarioRouter;
