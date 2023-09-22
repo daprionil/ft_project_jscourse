45b1e185011673c07b6a3ffc4cad5987a285d26d
daprionil <davidarenaoflol23@gmail.com>
2023-09-21 22:28:35 -0500

    Add cors and whiteList

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const rootRouter = require('./routes');
 
 //Create server
 const server = express();
+
+const whiteListCors = [
+    'http://localhost:5173'
+]
+const corsOptions = {
+    origin: function(origin, callback){
+        if(whiteListCors.includes(origin)){
+            callback(null, true);
+        }
+    }
+}
+server.use(cors(corsOptions));
 
 //! Define middlewares
 server.use(express.json());
